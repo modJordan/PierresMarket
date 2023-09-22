ff7dc3c474ce320e11fa9c397f06a0661bacb27c
modjordan <mod.jordan@icloud.com>
2023-09-21 19:41:07 -0700

    WIP

--- PierresMarket/Models/Order.cs
+++ PierresMarket/Models/Order.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace PierresMarket
+namespace PierresMarket.Models
 {
   public class Order
   {
--- PierresMarket/Models/Vendor.cs
+++ PierresMarket/Models/Vendor.cs
@@ -1,13 +1,16 @@
 using System.Collections.Generic;
 
 
-namespace PierresMarket
+namespace PierresMarket.Models
 {
   public class Vendor
   {
+    public int Id { get; }
     public string Name { get; }
     public string Description { get; }
     public List<Order> Orders { get; set; }
+
+    private static List<Vendor> _instances = new List<Vendor> { };
 
 
     public Vendor(string name, string description)
@@ -15,10 +18,22 @@ namespace PierresMarket
       Name = name;
       Description = description;
       Orders = new List<Order>();
+      _instances.Add(this);
+      Id = _instances.Count;
 
     }
 
+    public static List<Vendor> GetAll()
+    {
+      return _instances;
+    }
 
-
+    public static Vendor Find(int id)
+    {
+      return _instances[id - 1];
+    }
   }
+
+
+
 }
