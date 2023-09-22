40b167efadf311839a6ddd1be646971b7fc739b4
modjordan <mod.jordan@icloud.com>
2023-09-21 18:45:06 -0700

    Add failing test for Order

---
+++ PierresMarket.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,21 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PierresMarket;
+using System;
+
+namespace PierresMarket.Tests
+{
+  [TestClass]
+  public class OrderTests
+  {
+    [TestMethod]
+    public void NewOrder_CreatesInstance_OrderInstance()
+    {
+      //Arrange & Act
+      Order newOrder = new Order("Croissant Order", "Weekly supply of croissants.", 50.00, DateTime.Now);
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+
+      //Assert
+      Assert.IsInstanceOfType(newOrder, typeof(Order));
+    }
+  }
+}
